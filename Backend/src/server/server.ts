290df3ee89820c9fa1c0d5307cb40e7488b7fd61
Gustavo Moraes <guhms@outlook.com>
2025-04-11 21:03:24 -0300

    Conexao Postgres

--- Backend/src/server/server.ts
+++ Backend/src/server/server.ts
@@ -1,11 +1,19 @@
-import express from 'express';
+import express from "express";
+import pool from "./db";
 
+pool
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send('Hello HypoHeat!');
+server.get("/", (req, res) => {
+  res.send("Hello HypoHeat!");
 });
 
-
-export {server};
+export { server };
