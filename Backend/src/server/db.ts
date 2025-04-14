85b7c65f66538c2ad8b0c1166c1eebb0ee11c1e2
Carlos Magalhães <carlosedsmagalhaes@gmail.com>
2025-04-13 22:37:02 -0300

    Adicionado script para download e inserção dos dados

--- Backend/src/server/db.ts
+++ Backend/src/server/db.ts
@@ -2,9 +2,25 @@ import { Pool } from "pg";
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "hypoheat",
-  password: "1234",
+  database: "bdqueimadas",
+  password: "123",
   port: 5432,
 });
 
-export default pool;
+async function query(sql: string, params?: any[]) {
+  try {
+    const res = await pool.query(sql, params);
+    if (res.command == "INSERT") {
+      return res.rows[0];
+    } else if (res.command == "SELECT") {
+      return res.rows;
+    } else if (res.command == "DELETE" || res.command == "UPDATE") {
+      return { rowcount: res.rowCount };
+    } else {
+      return { sql };
+    }
+  } catch (e: any) {
+    return { message: e.message };
+  }
+}
+export { pool, query };
