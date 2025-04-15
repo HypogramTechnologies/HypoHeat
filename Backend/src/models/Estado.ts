0501172de144cd8394a8feee40069c444033801d
Andressa Toledo <109250362+andressatoledo@users.noreply.github.com>
2025-04-14 21:09:53 -0300

    Requisito de tecnicas de programação
    Entidades do DER (Banco de dados) em classe

---
+++ Backend/src/models/Estado.ts
@@ -0,0 +1,13 @@
+export class Estado {
+    estadoID: number;
+    estadoNome: string;
+    paisID: number;
+  
+    constructor(estadoID: number, estadoNome: string, paisID: number) {
+      this.estadoID = estadoID;
+      this.estadoNome = estadoNome;
+      this.paisID = paisID;
+    }
+
+  }
+  
---
+++ Backend/src/models/Municipio.ts
@@ -0,0 +1,13 @@
+export class Municipio {
+    municipioID: number;
+    municipioNome: string;
+    estadoID: number;
+  
+    constructor(municipioID: number, municipioNome: string, estadoID: number) {
+      this.municipioID = municipioID;
+      this.municipioNome = municipioNome;
+      this.estadoID = estadoID;
+    }
+  
+  }
+  
