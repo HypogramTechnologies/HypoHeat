33a764c12bd507839e870c9b49e44792d7ff03b0
Gustavo Moraes <guhms@outlook.com>
2025-04-24 02:04:11 -0300

    Criando rotas para os filtros

---
+++ Backend/src/server/controllers/FiltroController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { query } from "../models/db";
+
+class FiltroController {
+  public async Estados(_: Request, res: Response): Promise<void> {
+    const r: any = await query("SELECT estadonome FROM estado ");
+    res.json(r);
+  }
+
+  public async Biomas(_: Request, res: Response): Promise<void> {
+    const r: any = await query("SELECT biomanome FROM bioma ");
+    res.json(r);
+  }
+}
+export default new FiltroController();
