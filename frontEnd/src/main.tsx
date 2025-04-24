e5c09089d085fbe7320641dc23e022efaaca6114
Gustavo Moraes <guhms@outlook.com>
2025-04-24 01:26:37 -0300

    Ajuste mapa

--- frontEnd/src/main.tsx
+++ frontEnd/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 //import App from "./App.tsx";
 import Filtro from "./components/Filtro";
-import Mapa from "./components/Mapa"
+import Mapa from "./components/Mapa";
 import { FiltroProvider } from "./context/FiltroContext";
 import { CSSProperties } from "react";
 
@@ -45,41 +45,40 @@ const states = [
   "TO",
 ];
 
-
 const containerStyle: CSSProperties = {
-  display: 'flex',
-  height: '100vh',
+  display: "flex",
+  height: "100vh",
+  width: "100vw",
 };
 
 const filtroStyle: CSSProperties = {
-  width: '300px',
-  backgroundColor: '#1e1e1e',
-  padding: '16px',
-  overflowY: 'auto',
-  color: 'white',
+  width: "500px",
+  backgroundColor: "#1e1e1e",
+  padding: "16px",
+  overflowY: "auto",
+  color: "white",
 };
 
 const mapaWrapperStyle: CSSProperties = {
   flex: 1,
-  display: 'flex',
-  flexDirection: 'column',
+  display: "flex",
+  flexDirection: "column",
 };
 
 const mapaStyle: CSSProperties = {
   flex: 1,
-  width: '100%',
+  width: "100%",
 };
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <FiltroProvider>
-      <div style={containerStyle}>
-        <div style={filtroStyle}>
+    <div style={containerStyle}>
+      <div style={filtroStyle}>
+        <FiltroProvider>
           <Filtro biomes={biomes} states={states} />
-        </div>
-          <Mapa />
+        </FiltroProvider>
       </div>
-    </FiltroProvider>
+      <Mapa />
+    </div>
   </StrictMode>
 );
-
