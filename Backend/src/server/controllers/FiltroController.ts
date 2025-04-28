f93a2a3e95ca3b01b2aa7fbe296ebb3bcc3b2ae1
Carlos Magalhães <65139043+carlosedsmagalhaes@users.noreply.github.com>
2025-04-28 09:54:28 -0300

    Ordena Estados e biomas

--- Backend/src/server/controllers/FiltroController.ts
+++ Backend/src/server/controllers/FiltroController.ts
@@ -3,12 +3,12 @@ import { query } from "../models/db";
 
 class FiltroController {
   public async Estados(_: Request, res: Response): Promise<void> {
-    const r: any = await query("SELECT estadonome FROM estado");
+    const r: any = await query("SELECT estadonome FROM estado ORDER BY estadonome");
     res.json(r);
   }
 
   public async Biomas(_: Request, res: Response): Promise<void> {
-    const r: any = await query("SELECT biomanome  FROM bioma");
+    const r: any = await query("SELECT biomanome  FROM bioma ORDER BY biomanome");
     res.json(r);
   }
 }
