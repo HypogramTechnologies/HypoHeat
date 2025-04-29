631a9cb6a339f0b469a7c14290420c87c022a492
Carlos Magalhães <65139043+carlosedsmagalhaes@users.noreply.github.com>
2025-04-28 22:32:37 -0300

    Ajusta OcorrenciaController para que a consulta fique dinâmica

--- Backend/src/server/controllers/OcorrenciaController.ts
+++ Backend/src/server/controllers/OcorrenciaController.ts
@@ -1,11 +1,42 @@
 import { Request, Response } from "express";
 import { query } from "../models/db";
 
+interface Filtro {
+  bioma?: string;
+  estado?: string;
+  dataInicial?: string;
+  dataFinal?: string;
+}
+
 class OcorrenciaController {
-  public async Filtrar_foco_calor(_: Request, res: Response): Promise<void> {
-    const r: any = await query(
-      "SELECT ocorrencialatitude, ocorrencialongitude FROM view_ocorrencias WHERE ocorrenciadatahora BETWEEN '2025-04-10 00:10:00.000' AND '2025-04-20 00:10:00.000'"
-    );
+  public async Filtrar_foco_calor(req: Request, res: Response): Promise<void> {
+    const filtro: Filtro = req.body;
+    let consulta: string = "SELECT * FROM view_ocorrencias";
+    let condicoes: string[] = [];
+
+    if (filtro.estado) {
+      condicoes.push(`estadonome = '${filtro.estado}'`);
+    }
+
+    if (filtro.bioma) {
+      condicoes.push(`biomanome = '${filtro.bioma}'`);
+    }
+
+    if (filtro.dataInicial) {
+      condicoes.push(`ocorrenciadatahora >= '${filtro.dataInicial}'`);
+    }
+
+    if (filtro.dataFinal) {
+      condicoes.push(`ocorrenciadatahora <= '${filtro.dataFinal}'`);
+    }
+
+    if (condicoes.length > 0) {
+      consulta += " WHERE " + condicoes.join(" AND ");
+      console.log(consulta);
+    }
+
+    const r: any = await query(consulta);
+
     res.json(r);
   }
 }
